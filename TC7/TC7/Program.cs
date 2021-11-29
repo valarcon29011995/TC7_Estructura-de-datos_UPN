e3e7ae2ff28e52da058aced109a8223166ae074a
valarcon29011995 <92767134+valarcon29011995@users.noreply.github.com>
2021-11-29 12:03:36 -0500

    Add files via upload

---
+++ TC7/TC7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TC7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] graph = 
+            {
+                { 0,8,0,0,5,0,0 },
+                { 6,0,16,0,8,0,0},
+                { 0,0,0,10,0,5,9},
+                { 0,0,1,0,0,0,0},
+                { 5,8,0,0,0,0,6},
+                { 0,0,0,0,0,0,10},
+                { 0,0,0,0,6,5,0}
+            };
+            Prim.PrimAlgo(graph, 7);
+            Console.Read();
+        }
+    }
+}
